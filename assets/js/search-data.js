a6e022396be061cab7da510a7ff8d962b650dcc6
rafinrabbi <54761115+rafinrabbi@users.noreply.github.com>
2026-05-23 13:42:20 +0000

    Deploying to gh-pages from @ rafinrabbi/rafinrabbi.github.io@f97db4402e75b62c0d75a4430bd9fbe332c9b300 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -494,13 +494,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/rawhatur-rabbi-rafin", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
